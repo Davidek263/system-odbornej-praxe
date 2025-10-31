d89c5b3bd750895c1dff0bb46d27148e0a4aba65
J4MB0 <peterjambo4@gmail.com>
2025-10-31 14:47:14 +0100

    UI dashboard firma + State diagram

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,14 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ForgotPasswordView from '../views/ForgotPasswordView.vue'
-import SetPasswordView from '../views/SetPassword.vue' // 👈 pridaj tento import
+import FirmaView from '../views/FirmaView.vue'
+import SetPasswordView from '../views/SetPassword.vue' 
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', name: 'login', component: LoginView },
   { path: '/register', name: 'register', component: RegisterView },
   { path: '/forgot-password', name: 'forgot', component: ForgotPasswordView },
-{ path: '/set-password', name: 'setpassword', component: SetPasswordView }
+  { path: '/firmaview', name: 'firmaview', component: FirmaView},
+  { path: '/set-password', name: 'setpassword', component: SetPasswordView }
 ]
 
 const router = createRouter({
