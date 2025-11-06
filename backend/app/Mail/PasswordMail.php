590cad36dee0a5b9f006620ddef98190139c943b
Davidek263 <david.belan263@gmail.com>
2025-11-06 12:54:19 +0100

    Pridane maily na dočasné heslo a aktiváciu

--- backend/app/Mail/PasswordMail.php
+++ backend/app/Mail/PasswordMail.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MailSender extends Mailable
+class PasswordMail extends Mailable
 {
     use Queueable, SerializesModels;
 
