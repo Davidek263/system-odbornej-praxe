47bd8b89c4722cd51779cc2c91589cfa813c51b0
TomasLacuska <smailimansk@gmail.com>
2026-01-11 18:48:40 +0100

    treba odstrániť CSV kód z garantordashboardu a polish statistics, export okno je tam not good.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -59,6 +59,9 @@
           <RouterLink to="/guarantor-dashboard/tokens" class="nav-item" active-class="active-link">
             Tokeny
           </RouterLink>
+          <RouterLink to="/guarantor-dashboard/statistics" class="nav-item" active-class="active-link">
+            Štatistiky
+          </RouterLink>
           <RouterLink to="/profile" class="nav-item" active-class="active-link">
             Profil
           </RouterLink>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,7 @@ import StudentDashboard from '../views/StudentViews/StudentDashboard.vue'
 import SetPasswordView from '../views/SetPassword.vue'
 import GuarantorDashboard from '../views/GuarantorViews/GuarantorDashboard.vue'
 import GuarantorTokens from '@/views/GuarantorViews/GuarantorTokens.vue'
+import GuarantorStatistics from '@/views/GuarantorViews/GuarantorStatistics.vue'
 import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
@@ -22,6 +23,7 @@ const routes = [
   { path: '/student-dashboard', name: 'studentdashboard', component: StudentDashboard, meta: { requiresAuth: true, roles: ['student']} },
   { path: '/guarantor-dashboard', name: 'guarantordashboard', component: GuarantorDashboard, meta: { requiresAuth: true, roles: ['guarantor']} },
   { path: '/guarantor-dashboard/tokens', name: 'guarantortokens', component: GuarantorTokens, meta: { requiresAuth: true, roles: ['guarantor']} },
+  { path: '/guarantor-dashboard/statistics', name: 'guarantorstatistics', component: GuarantorStatistics, meta: { requiresAuth: true, roles: ['guarantor']} },
   { path: '/set-password', name: 'setpassword', component: SetPasswordView },
   { path: '/profile', name: 'profile', component: ProfileView, meta: { requiresAuth: true }},
 ]
